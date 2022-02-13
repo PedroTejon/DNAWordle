9ba5ba59b6bb250b73768d3b0484ed93a238fd4e
PedroTejon <pedrohtejon@gmail.com>
2022-02-13 13:22:56 -0300

    Fix: Erros de Key no console removidos

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -2,29 +2,24 @@ import React from 'react';
 import { Key } from './Key';
 
 export class Keyboard extends React.Component{
+    createRow(keyLayout){
+        let keys = []
+        for (let n of keyLayout){
+            keys.push(<Key onClick={() => this.props.keyHandler(n)} key={n} letter={n}/>)
+        }
+        return keys
+    }
     render() {
         return (
             <div style={{display:'flex', flexDirection:'column', alignItems:'center', gap:'1vh', marginTop:'3vh'}}>
                 <div style={{display:'flex', flexDirection:'row', gap:'1vh'}}>
-                    {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map(
-                        (n) => {
-                            return <Key onClick={() => this.props.keyHandler(n)} letter={n}/>
-                        })
-                    }
+                    {this.createRow(['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'])}
                 </div>
                 <div style={{display:'flex', flexDirection:'row', gap:'1vh'}}>
-                    {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].map(
-                            (n) => {
-                                return <Key onClick={() => this.props.keyHandler(n)} letter={n}/>
-                        })
-                    }
+                    {this.createRow(['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'])}
                 </div>
                 <div style={{display:'flex', flexDirection:'row', gap:'1vh'}}>
-                    {['↤', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '←'].map(
-                            (n) => {
-                                return <Key onClick={() => this.props.keyHandler(n)} letter={n}/>
-                        })
-                    }
+                    {this.createRow(['↤', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '←'])}
                 </div>
             </div>
         )
